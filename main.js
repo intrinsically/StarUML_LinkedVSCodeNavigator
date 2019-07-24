31230bee940a83db056c84e2a711f7dcf65890ec
Andrew McVeigh <andrew.x.mcveigh@gmail.com>
2019-07-24 00:47:41 -0700

    no message

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function _openLinkedVSCode(element) {
           "Basic " + btoa("amcveigh:tiggle")
         );
       },
-      data: '"src/genbase.ts"',
+      data: JSON.stringify({ resource: "src/genbase.ts" }),
       success: function(data) {
         console.log(data);
       },
